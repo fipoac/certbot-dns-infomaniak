7813df37ef7a247ecdb39e015ec3746f2c3c9b39
fipoac <contact-fipoac@cc.ioa.pm>
2024-06-06 20:45:30 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+cat << 'EOF' >> /var/spool/cron/crontabs/certbot
+30 3 * * 7 certbot renew
+EOF
+certbot certonly --authenticator dns-infomaniak $@
+echo "Copying cert und key to /cert
+cp /etc/letsencrypt/live/$CERT_NAME/fullchain.pem /cert
+cp /etc/letsencrypt/live/$CERT_NAME/privkey.pem /cert
+echo "Starting cron for autorenewals"
+exec crond -fL /var/log/cron.log
