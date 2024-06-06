802eca7026c96872f117c43f376aae9b9a44165c
Nico Fischer <nico.fischer@recos-ic.ch>
2024-06-06 21:08:48 +0200

    updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 cat << 'EOF' >> /var/spool/cron/crontabs/certbot
 30 3 * * 7 certbot renew
-'EOF'
+EOF
 certbot certonly --authenticator dns-infomaniak "$@"
-echo "Copying cert und key to /cert
+echo "Copying cert und key to /cert"
 cp -f /etc/letsencrypt/live/$CERT_NAME/fullchain.pem /cert
 cp -f /etc/letsencrypt/live/$CERT_NAME/privkey.pem /cert
 echo "Starting cron for autorenewals"
