516c382066f32aab2e5bd8408e0da96705c0adef
fipoac <contact-fipoac@cc.ioa.pm>
2024-06-06 21:02:11 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 cat << 'EOF' >> /var/spool/cron/crontabs/certbot
 30 3 * * 7 certbot renew
-EOF
-certbot certonly --authenticator dns-infomaniak $@
+'EOF'
+certbot certonly --authenticator dns-infomaniak "$@"
 echo "Copying cert und key to /cert
 cp -f /etc/letsencrypt/live/$CERT_NAME/fullchain.pem /cert
 cp -f /etc/letsencrypt/live/$CERT_NAME/privkey.pem /cert
