450cba0a8efaddd90edc6e831f00403c54f678bc
Nico Fischer <nico.fischer@recos-ic.ch>
2024-06-07 16:37:35 +0200

    updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 set -e
-echo "30 3 * * 6 certbot renew && cp -f /etc/letsencrypt/live/$CERT_NAME/fullchain.pem /cert && cp -f /etc/letsencrypt/live/$CERT_NAME/privkey.pem /cert" | crontab -
-if [ -e /etc/letsencrypt/live/$CERT_NAME/fullchain.pem ] && [ -s /etc/letsencrypt/live/$CERT_NAME/fullchain.pem ]; then
-  echo "Certificate already installed"
-else
-  echo "Requesting certificate"
-	output=$(certbot certonly --logs-dir /tmp/certbot --authenticator dns-infomaniak --cert-name $CERT_NAME "$@")
-  echo "Copying cert und key to /cert"
-  CERT_PATH=$(echo "$output" | grep -o '/.*fullchain\.pem')
-  echo $output
-  export CERT_NAME=$(basename $(dirname $CERT_PATH))
-  cp -f /etc/letsencrypt/live/$CERT_NAME/fullchain.pem /cert
-  cp -f /etc/letsencrypt/live/$CERT_NAME/privkey.pem /cert
-fi
-echo "Starting cron for autorenewals"
-exec crond -f
+# echo "30 3 * * 6 certbot renew && cp -f /etc/letsencrypt/live/$CERT_NAME/fullchain.pem /cert && cp -f /etc/letsencrypt/live/$CERT_NAME/privkey.pem /cert" | crontab -
+# if [ -e /etc/letsencrypt/live/$CERT_NAME/fullchain.pem ] && [ -s /etc/letsencrypt/live/$CERT_NAME/fullchain.pem ]; then
+#   echo "Certificate already installed"
+# else
+#   echo "Requesting certificate"
+# 	output=$(certbot certonly --authenticator dns-infomaniak --cert-name $CERT_NAME "$@")
+#   echo "Copying cert und key to /cert"
+#   CERT_PATH=$(echo "$output" | grep -o '/.*fullchain\.pem')
+#   echo $output
+#   export CERT_NAME=$(basename $(dirname $CERT_PATH))
+#   cp -f /etc/letsencrypt/live/$CERT_NAME/fullchain.pem /cert
+#   cp -f /etc/letsencrypt/live/$CERT_NAME/privkey.pem /cert
+# fi
+# echo "Starting cron for autorenewals"
+# exec crond -f
+certbot certonly --authenticator dns-infomaniak --cert-name $CERT_NAME "$@"
