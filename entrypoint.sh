3accf3a0be9f013e6cc0e977cc729f593f7c8f80
Nico Fischer <nf@ioa.pm>
2024-06-16 17:04:29 +0200

    changed entrypoint.sh to use different cp command

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,15 @@ if [ ! -s /cert/fullchain.pem ] || [ ! -s /cert/privkey.pem ]; then
   CERT_PATH=$(echo "$output" | grep -o '/.*fullchain\.pem')
   echo $output
   export CERT_NAME=$(basename $(dirname $CERT_PATH))
-  cp -f /etc/letsencrypt/live/$CERT_NAME/fullchain.pem /cert
-  cp -f /etc/letsencrypt/live/$CERT_NAME/privkey.pem /cert
+  cp /etc/letsencrypt/live/$CERT_NAME/fullchain.pem /cert
+  if [ $? != 0 ]; then
+    echo "Tried to copy fullchain.pem: $? - Unsuccessful"
+  fi
+  cp /etc/letsencrypt/live/$CERT_NAME/privkey.pem /cert
+  if [ $? != 0 ]; then
+    echo "Tried to copy privkey.pem: $? - Unsuccessful"
+  fi
+
 fi
 echo "Starting cron for autorenewals"
 exec crond -f
