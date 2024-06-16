e8a716438f08227780aa785acee1bb49f4b204e2
Nico Fischer <nf@ioa.pm>
2024-06-16 16:42:41 +0200

    fixed entrypoint.sh not copying certificate to /cert on restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
 echo "30 3 * * 6 certbot renew && cp -f /etc/letsencrypt/live/$CERT_NAME/fullchain.pem /cert && cp -f /etc/letsencrypt/live/$CERT_NAME/privkey.pem /cert" | crontab -
-if [ -e /etc/letsencrypt/live/$CERT_NAME/fullchain.pem ] && [ -s /etc/letsencrypt/live/$CERT_NAME/fullchain.pem ]; then
+if [ -f /etc/letsencrypt/live/$CERT_NAME/fullchain.pem ] && [ -f /etc/letsencrypt/live/$CERT_NAME/fullchain.pem ]; then
   echo "Certificate already installed"
 else
   echo "Requesting certificate"
 	output=$(certbot certonly -n --authenticator dns-infomaniak --cert-name $CERT_NAME "$@")
+fi
+if [ ! -s /cert/fullchain.pem ] || [ ! -s /cert/privkey.pem ]; then
   echo "Copying cert und key to /cert"
   CERT_PATH=$(echo "$output" | grep -o '/.*fullchain\.pem')
   echo $output
